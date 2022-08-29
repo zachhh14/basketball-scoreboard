a95410cff5ff56dcdc80023d566514e823b9d4df
zachhh14 <zactec115@gmail.com>
2022-08-29 22:19:37 +0800

    Finish

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = document.getElementById("num1")
+let guestScore = document.getElementById("num2")
+let starthScore = 0
+let startgScore = 0
+
+// Home score
+function homePlusone(){
+    console.log("Button 1 Clicked")
+    starthScore++
+    homeScore.textContent = starthScore
+}
+
+function homePlustwo(){
+
+    starthScore+=2
+    homeScore.textContent = starthScore
+}
+
+function homePlusthree(){
+   
+    starthScore+=3
+    homeScore.textContent = starthScore
+}
+
+// Guest score
+function guestPlusone(){
+    console.log("Button 1 Clicked")
+    startgScore++
+    guestScore.textContent = startgScore
+}
+
+function guestPlustwo(){
+
+    startgScore+=2
+    guestScore.textContent = startgScore
+}
+
+function guestPlusthree(){
+   
+    startgScore+=3
+    guestScore.textContent = startgScore
+}
